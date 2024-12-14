65bb27b961af27d8442d1e9956feeb850d2928c6
Islam Hany <islam2014hany@gmail.com>
2024-12-14 03:38:56 +0200

    add SearchBar component in homepage

--- front/src/homePage/homePage.jsx
+++ front/src/homePage/homePage.jsx
@@ -9,6 +9,7 @@ import Filter from "../Filter/Filter";
 import SearchSort from "../SearchAndSort/SearchAndSort";
 import EmailTable from "../EmailTable/EmailTable";
 import MenuBar from "../MenuBar/MenuBar";
+import SearchBar from "../SearchAndSort/SearchBar";
 function HomePage({emails , user}){
           
     return (<>
