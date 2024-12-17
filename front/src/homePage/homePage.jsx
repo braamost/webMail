5548fa1991220058de1cd901628be2c18f8fd800
braa mostafa <braamostafa1@gmail.com>
2024-12-17 06:19:05 +0200

    emails loaded successfully✔ still the attachments

--- front/src/homePage/homePage.jsx
+++ front/src/homePage/homePage.jsx
@@ -1,35 +1,37 @@
-import "./HomePage.css" 
+import "./HomePage.css";
 
 import MenuBar from "../MenuBar/MenuBar";
 import SearchBar from "../SearchAndSort/SearchBar";
 import EmailTable from "../IsTable/EmailTable";
-import {MocData} from "../MocData.jsx"
+import { MocData } from "../MocData.jsx";
 import { useState } from "react";
 import EmailPage from "../EmailPage/EmailPage.jsx";
 
-function HomePage({emails , user, error}){
-    const [emailPage ,setEmailPage] = useState(false);
-    const [selectedEmail, setSelectedEmail] = useState({});
+function HomePage({ emails, user, error }) {
+  const [emailPage, setEmailPage] = useState(false);
+  const [selectedEmail, setSelectedEmail] = useState({});
 
-
-      const StoringEmailSelected = (email) => {
-        setSelectedEmail(email);
-      }
-    return (
-        <>
-        <div className="pagecontent">
-          {emailPage ? (
-            <EmailPage email={selectedEmail} callback={setEmailPage} />
-          ) : (
-            <>
-              <SearchBar />
-              <EmailTable emails={MocData} callback={setEmailPage} FuncEmailPage={StoringEmailSelected}/>
-            </>
-          )}
-          <MenuBar user={user} />
-        </div>
-      </>
-    )
-
+  const StoringEmailSelected = (email) => {
+    setSelectedEmail(email);
+  };
+  return (
+    <>
+      <div className="pagecontent">
+        {emailPage ? (
+          <EmailPage email={selectedEmail} callback={setEmailPage} />
+        ) : (
+          <>
+            <SearchBar />
+            <EmailTable
+              emails={emails}
+              callback={setEmailPage}
+              FuncEmailPage={StoringEmailSelected}
+            />
+          </>
+        )}
+        <MenuBar user={user} />
+      </div>
+    </>
+  );
 }
-export default HomePage
+export default HomePage;
